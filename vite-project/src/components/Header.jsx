5d5f76ce2f1e01f89b6c513cf74d9af32814b943
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-01 13:13:37 +0200

    "Added Header component with React and react-router-dom imports, and defined activeStyles object."

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link, NavLink } from "react-router-dom"
+
+export default function Header() {
+    const activeStyles = {
+        fontweight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
+}
