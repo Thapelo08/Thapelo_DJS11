a7f375932ae14c0ba599fc07f4aa0b853c03eda3
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-08 15:05:45 +0200

    Add podcast genres mapping

---
+++ vite-project/src/pages/Favorites.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+
+const genres = {
+    1: "Personal Growth",
+    2: "Investigate Journalism",
+    3: "History",
+    4: "Comedy",
+    5: "Entertainment",
+    6: "Business",
+    7: "Fiction",
+    8: "News",
+    9: "Kids and Family",
+};
+
