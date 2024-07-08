2571cf76b9f92c5804e706920a8f03d5dc697c7b
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-08 11:34:30 +0200

    "Added location and navigate hooks, and implemented handleSubmit function in Login component"

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -7,5 +7,22 @@ export default function Login() {
     const [loginFormData, setLoginDataForm] = React.useState({ email: "", password: ""})
     const [status, setStatus] = React.useState("idle")
     const [error, setError] = React.useState(null)
-    
+
+    const location = useLocation()
+    const navigate = useNavigate()
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setStatus("pending")
+        loginUser(loginFormData)
+           .then(data => {
+            setError(null)
+            localStorage.setItem("loggedin", true)
+            navigate(from, { replace: true })
+           })
+           .catch(err => {
+            setError(err)
+           })
+    }
 }
