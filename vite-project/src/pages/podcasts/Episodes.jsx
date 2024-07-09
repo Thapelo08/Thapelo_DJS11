3e1ecf5b7acd76dc6dfabbbe094269dd2529eb8a
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-09 13:45:53 +0200

    "Added Episodes component skeleton with state and useParams"

--- vite-project/src/pages/podcasts/Episodes.jsx
+++ vite-project/src/pages/podcasts/Episodes.jsx
@@ -3,3 +3,12 @@ import { useParams, Link } from "react-router-dom";
 import { getEpisodesBySeason } from "../../api"
 import Audio from './Audio';
 
+const Episodes = () => {
+    const { id, season } = useParams();
+    const [episodes, setEpisodes ] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [fetchErrror, setFetchError] = useState(null);
+
+
+}
+
