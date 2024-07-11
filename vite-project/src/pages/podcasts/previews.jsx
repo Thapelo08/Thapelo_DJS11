5d64673de9addb5f5f443d17d21e9fad3dae8ae6
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-11 10:45:32 +0200

    "Added genres object and loadPodcasts function to Previews component"

--- vite-project/src/pages/podcasts/previews.jsx
+++ vite-project/src/pages/podcasts/previews.jsx
@@ -15,4 +15,34 @@ const Previews = () => {
 
     const { isFavorite, addFavorite, removeFavorite } = useFavorites();
     const { searchHistory, addSearchQuery, clearSearchHistory } = useSearchHistory();
+  
+    const genres = {
+        1: "Personal Growth",
+        2: "Investigative Journalism",
+        3: "History",
+        4: "Comedy",
+        5: "Entertainment",
+        6: "Business",
+        7: "Fiction",
+        8: "News",
+        9: "Kids and Family",
+      };
+
+      useEffect(() => {
+        const loadPodcasts = async () => {
+            try {
+                const data = await getPreviews();
+                setPodcasts(data);
+                setDisplayedPodcasts(data);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error fetching podacasts:", error);
+                setFetchError("Failed to retrieve podcast previews.");
+                setIsLoading(false);
+            }
+        };
+
+        loadPodcasts();
+      }, []);
+
 }
