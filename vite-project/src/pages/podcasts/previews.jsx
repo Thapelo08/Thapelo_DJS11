694907a33dc84b0c616118bec63982a05b6b5093
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-11 10:31:47 +0200

    Refactoring the Previews component to use useState for managing component state

---
+++ vite-project/src/pages/podcasts/previews.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { getPreviews } from "../../api";
+import useFavorites from "../../useFavorites";
+import useSearchHistory from ""
+
+const Previews = () => {
+    const [podcasts, setPodcasts] = useState([]);
+    const [displayedPodcasts, setDisplayedPodcasts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
+    const [fetchError, setFetchError] = useState(null);
+    const [sortOption, setSortOption] = useState("All");
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedGenre, setSelectedGenre] = useState("All");
+}
