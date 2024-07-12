b37718ed32f091ba05bde741f66d6c9d911d46eb
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-12 14:16:16 +0200

    Added eslint-disable no-unused-vars comment to multiple components and updated a string in Home.jsx to use an apostrophe entity instead of a single quote.

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react"
 import { Link } from "react-router-dom"
 
@@ -5,7 +6,7 @@ export default function Home() {
     return (
         <div  className="home-container">
             <h1>THAPELO PODCAST</h1>
-            <p>Welcome to these Great Podcast,Where you enjoy your favorites. You won't get bored right after step into it.  </p>
+            <p>Welcome to these Great Podcast,Where you enjoy your favorites. You &apos get bored right after step into it.  </p>
             <Link to="/podcasts" className="btn btn-primary">Get Started</Link>
         </div> 
     )
